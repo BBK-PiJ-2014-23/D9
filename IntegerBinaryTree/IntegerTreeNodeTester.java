a424d35a9023f448500b09491d5b7916e0d85b80
unknown <stefanm@LONWRK07474.eidos.com>
2014-11-25 14:27:05 +0000

    Added tests for print methods.

--- IntegerBinaryTree/IntegerTreeNodeTester.java
+++ IntegerBinaryTree/IntegerTreeNodeTester.java
@@ -1,11 +1,13 @@
 public class IntegerTreeNodeTester
 {
     public static void main (String[] args) {
-        int[] array = {8, 3, 5, 2, 7};
-        IntegerTreeNode tree = ListUtilities.arrayToTreeNode(10, array);
+        int[] array = {9, 5, 3};
+        IntegerTreeNode tree = ListUtilities.arrayToTreeNode(6, array);
         System.out.println(tree.contains(11));
         System.out.println(tree.contains(5));
         System.out.println(tree.getMax());
         System.out.println(tree.getMin());
+        System.out.println(tree.toString());
+        System.out.println(tree.toStringSimple());
     }
 }
